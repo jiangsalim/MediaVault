031a12259a0034d0a6acf99d18980b3c1ce77e4d
jiangsalim <jaingsalim@gmail.com>
2026-05-13 21:59:27 +0300

    Website files in correct location, cleaned mobile folder

--- mobile/next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: { domains: ['i.ytimg.com', 'img.youtube.com'] },
-  async headers() {
-    return [{ source: '/api/:path*', headers: [{ key: 'Access-Control-Allow-Origin', value: '*' }] }];
-  }
-};
-module.exports = nextConfig;
